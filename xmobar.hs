d76ec0bef2c0bdd9c135828fa59b4783c8f2a89b
Thomas Dziedzic <gostrc@gmail.com>
2012-06-26 15:16:41 -0500

    make room for a tray with xmobar

--- xmobar.hs
+++ xmobar.hs
@@ -2,7 +2,8 @@ Config { font = "xft:ProFont:pixelsize=11"
        , bgColor = "black"
        --, fgColor = "grey"
        , fgColor = "lightskyblue"
-       , position = Top
+       --, position = Top
+       , position = Static { xpos = 0 , ypos = 0, width = 1205, height = 18 }
        , lowerOnStart = True
        , commands = [ --Run Weather "KORD" ["-t","<station>: <tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
                       Run Weather "KORD" ["-t","O'Hare: <tempC>C","-L","18","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
@@ -13,10 +14,10 @@ Config { font = "xft:ProFont:pixelsize=11"
                     , Run Swap ["-L","30","-H","70","--normal","yellow","--high","red","--low","green"] 10
                     , Run Battery ["-t","Batt: <left>%","-L","30","-H","70","--normal","yellow","--high","green","--low","red", "--", "-c", "energy_full"] 10 -- remove after "--" once upgraded to > xmobar > 0.14
                     , Run StdinReader
-                    , Run Com "uname" ["-s","-r"] "" 36000
+                    --, Run Com "uname" ["-s","-r"] "" 36000
                     , Run Date "%a %b %_d %Y %H:%M:%S" "date" 10
                     ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% | %cpu% | %memory% * %swap% | %wlan0% | %battery%}{ <fc=#ee9a00>%date%</fc>| %KORD% | <fc=#35D699>%uname%</fc>"
+       , template = "%StdinReader% | %cpu% | %memory% * %swap% | %wlan0% | %battery%}{ <fc=#ee9a00>%date%</fc>| %KORD%"
        }
